8cd69400c0143e1979961423e94b5ceffad3781f
lucialv <lucia.alvrzt@gmail.com>
2024-10-23 00:34:15 +0200

    changed comments

--- src/pages/pats/index.ts
+++ src/pages/pats/index.ts
@@ -1,16 +1,16 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = ({ request }) => {
-  const totalGifs = 30; // Total de GIFs disponibles
-  const randomNumber = Math.floor(Math.random() * totalGifs) + 1; // Número aleatorio entre 1 y 30
+  const totalGifs = 30;
+  const randomNumber = Math.floor(Math.random() * totalGifs) + 1;
   const gifUrl = `https://cdn.lucia-dev.com/pat${randomNumber
     .toString()
-    .padStart(2, "0")}.gif`; // URL del GIF
+    .padStart(2, "0")}.gif`; // URL of the GIF
 
-  // Devuelve un objeto Response con JSON
+  // Return the URL of the GIF
   return new Response(JSON.stringify({ url: gifUrl }), {
     headers: {
-      "Content-Type": "application/json", // Asegúrate de que el encabezado de contenido sea JSON
+      "Content-Type": "application/json",
     },
   });
 };
