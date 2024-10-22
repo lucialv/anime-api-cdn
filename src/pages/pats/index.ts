b9f10ca2d32a825c541bd527a6bdbedcdc3194d6
lucialv <lucia.alvrzt@gmail.com>
2024-10-23 00:29:29 +0200

    now working with fetch

--- src/pages/pats/index.ts
+++ src/pages/pats/index.ts
@@ -1,12 +1,13 @@
-import type { APIRoute } from "astro";
+import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = async ({ redirect }) => {
+export const GET: APIRoute = ({ request }) => {
   const totalGifs = 30; // Total de GIFs disponibles
   const randomNumber = Math.floor(Math.random() * totalGifs) + 1; // Número aleatorio entre 1 y 30
-  const gifUrl = `https://cdn.lucia-dev.com/pat${randomNumber
-    .toString()
-    .padStart(2, "0")}.gif`; // URL del GIF
+  const gifUrl = `https://cdn.lucia-dev.com/pat${randomNumber.toString().padStart(2, '0')}.gif`; // URL del GIF
 
-  // Redirige al archivo de imagen GIF directamente
-  return redirect(gifUrl, 302);
+  return {
+    body: {
+      url: gifUrl, // Devuelve la URL en un objeto JSON
+    },
+  };
 };
